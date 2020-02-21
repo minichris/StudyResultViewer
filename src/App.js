db202c7a26b0210020deef13f5181320a135a5b9
Zaperox <admin@carolineleeceramics.co.uk>
2020-02-21 17:25:15 +0000

    Created system for ignoring failed tasks

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default class App extends React.Component {
 	};
 
 	
-	buttonClick(){
+	makeClipboard(onlyShowCompleted){
 		function updateClipboard(newClip) {
 			navigator.clipboard.writeText(newClip).then(function() {
 				console.log("Writing to clipboard successful");
@@ -40,27 +40,35 @@ export default class App extends React.Component {
 
 		const { selectedOption } = this.state;
 		if(selectedOption){
-			let taskData = this.props.Data.flatMap(participant => participant.clientData);
-			const taskAname = "Task 2A".slice(0, -1).concat("A");
-			const taskBname = "Task 2B".slice(0, -1).concat("B");
-			const taskATimes = taskData.filter(task => task.DisplayTitle == taskAname).flatMap(task => task.TimeSpentOnTask / 1000);
-			const taskBTimes = taskData.filter(task => task.DisplayTitle == taskBname).flatMap(task => task.TimeSpentOnTask / 1000);
-			let copyString = "GDPWiki\tGDPVis\r\n";
-			if(taskATimes.length != taskBTimes.length){
-				updateClipboard("Lengths don't match!");
-			}
-			else{
-				for(let i = 0; i < taskATimes.length; i++){
-					copyString += taskATimes[i] + "\t" + taskBTimes[i] + "\r\n";
-				
+			const currentTaskName = selectedOption.value.slice(0, -1); //get the task name without the letter
+			let taskData = this.props.Data.flatMap(participant => participant.clientData); //get all the task data
+			const allParticipants = new Set(taskData.flatMap(task => task.Participant)); //get an array of all the participant ids
+			let copyString = "GDPWiki" + " (" + currentTaskName + ")\tGDPVis (" + currentTaskName + ")\r\n"; //set up the heading on the copystring
+			allParticipants.forEach(function(paticpantID){ //for each participant
+				const taskAData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("A") && task.Participant == paticpantID);
+				const taskBData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("B") && task.Participant == paticpantID);
+				if(onlyShowCompleted){
+					let taskACompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("A")).completed === "true";
+					let taskBCompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("B")).completed === "true";
+					if(!(taskACompleted && taskBCompleted)){
+						console.log(paticpantID + " failed to complete one or more of the two tasks being copied, ignoring.");
+						return;
+					}
 				}
-				updateClipboard(copyString);
-			}
+				const taskATime = taskAData.TimeSpentOnTask / 1000; //get task A's time
+				const taskBTime = taskBData.TimeSpentOnTask / 1000; //get task B's time
+				copyString += taskATime + "\t" + taskBTime + "\r\n"; //add a newline
+			});
+			updateClipboard(copyString);
 		}
 		else{
 			console.log("No option selected");
 		}
 	}
+
+
+
+
 	
 	render(){
 		let tasks = this.props.Data.flatMap(participant => participant.clientData);
@@ -71,7 +79,8 @@ export default class App extends React.Component {
 		return (
 			<>
 				<Select value={selectedOption} onChange={this.handleChange.bind(this)} options={this.options()} />
-				<button onClick={this.buttonClick.bind(this)}>Copy shown tasks timings</button>
+				<button onClick={() => {this.makeClipboard(false)}}>Copy all shown tasks timings</button>
+				<button onClick={() => {this.makeClipboard(true)}}>Copy only shown tasks timings which completed</button>
 				<Tasks tasks={tasks} />
 			</>
 		);
