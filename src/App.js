d3efedcb5e987e0c8134159d94f539e95faf6e65
Zaperox <admin@carolineleeceramics.co.uk>
2020-03-09 18:31:00 +0000

    Added some functionality

--- src/App.js
+++ src/App.js
@@ -28,16 +28,47 @@ export default class App extends React.Component {
 		this.forceUpdate();
 	};
 
-	
-	makeClipboard(onlyShowCompleted){
-		function updateClipboard(newClip) {
-			navigator.clipboard.writeText(newClip).then(function() {
-				console.log("Writing to clipboard successful");
-			}, function() {
-				console.log("Writing to clipboard failed");
+	getDifference(){
+		const { selectedOption } = this.state;
+		if(selectedOption){
+			const currentTaskName = selectedOption.value.slice(0, -1); //get the task name without the letter
+			let taskData = this.props.Data.flatMap(participant => participant.clientData); //get all the task data
+			const allParticipants = new Set(taskData.flatMap(task => task.Participant)); //get an array of all the participant ids
+			let allDiffs = [];
+			allParticipants.forEach(function(paticpantID){ //for each participant
+				const taskAData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("A") && task.Participant == paticpantID);
+				const taskBData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("B") && task.Participant == paticpantID);
+				const taskACompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("A")).completed === "true";
+				const taskBCompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("B")).completed === "true";
+				if(!(taskACompleted && taskBCompleted)){
+					console.log(paticpantID + " failed to complete one or more of the two tasks being copied, ignoring.");
+					return;
+				}
+				const taskATime = taskAData.TimeSpentOnTask / 1000; //get task A's time
+				const taskBTime = taskBData.TimeSpentOnTask / 1000; //get task B's time
+				var diff = Math.abs(taskATime - taskBTime);
+				allDiffs.push(diff);
 			});
+			var total = 0;
+			for(var i = 0; i < allDiffs.length; i++) {
+				total += allDiffs[i];
+			}
+			this.updateClipboard( total / allDiffs.length );
 		}
+		else{
+			this.updateClipboard( "No selected option" );
+		}
+	}
 
+	updateClipboard(newClip) {
+		navigator.clipboard.writeText(newClip).then(function() {
+			console.log("Writing to clipboard successful");
+		}, function() {
+			console.log("Writing to clipboard failed");
+		});
+	}
+	
+	makeClipboard(onlyShowCompleted, onlyShowIfCompletedBoth = true){
 		const { selectedOption } = this.state;
 		if(selectedOption){
 			const currentTaskName = selectedOption.value.slice(0, -1); //get the task name without the letter
@@ -47,19 +78,39 @@ export default class App extends React.Component {
 			allParticipants.forEach(function(paticpantID){ //for each participant
 				const taskAData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("A") && task.Participant == paticpantID);
 				const taskBData = taskData.find(task => task.DisplayTitle == currentTaskName.concat("B") && task.Participant == paticpantID);
+				const taskACompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("A")).completed === "true";
+				const taskBCompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("B")).completed === "true";
 				if(onlyShowCompleted){
-					let taskACompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("A")).completed === "true";
-					let taskBCompleted = global.CheckboxData.find(d => d.participant == paticpantID && d.task == currentTaskName.concat("B")).completed === "true";
-					if(!(taskACompleted && taskBCompleted)){
+					if(!(taskACompleted && taskBCompleted) && onlyShowIfCompletedBoth){
 						console.log(paticpantID + " failed to complete one or more of the two tasks being copied, ignoring.");
 						return;
 					}
 				}
 				const taskATime = taskAData.TimeSpentOnTask / 1000; //get task A's time
 				const taskBTime = taskBData.TimeSpentOnTask / 1000; //get task B's time
-				copyString += taskATime + "\t" + taskBTime + "\r\n"; //add a newline
+				if(onlyShowIfCompletedBoth){
+					copyString += taskATime + "\t" + taskBTime + "\r\n"; //add a newline
+				}
+				else{
+					if(!onlyShowIfCompletedBoth){
+						if(taskACompleted){
+							copyString += taskATime;
+						}
+						else{
+							copyString += "Failed";
+						}
+						copyString += "\t";
+						if(taskBCompleted){
+							copyString += taskBTime;
+						}
+						else{
+							copyString += "Failed";
+						}
+						copyString += "\r\n";
+					}
+				}
 			});
-			updateClipboard(copyString);
+			this.updateClipboard(copyString);
 		}
 		else{
 			console.log("No option selected");
@@ -80,7 +131,9 @@ export default class App extends React.Component {
 			<>
 				<Select value={selectedOption} onChange={this.handleChange.bind(this)} options={this.options()} />
 				<button onClick={() => {this.makeClipboard(false)}}>Copy all shown tasks timings</button>
-				<button onClick={() => {this.makeClipboard(true)}}>Copy only shown tasks timings which completed</button>
+				<button onClick={() => {this.makeClipboard(true, false)}}>Copy only shown tasks timings which completed (showing faileds)</button>
+				<button onClick={() => {this.makeClipboard(true)}}>Copy only shown tasks timings which completed (ignore participants who failed at least one)</button>
+				<button onClick={() => {this.getDifference()}}>Get interpair difference</button>
 				<Tasks tasks={tasks} />
 			</>
 		);
